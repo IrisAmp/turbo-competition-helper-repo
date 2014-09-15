0f356dd131e1d11b8b02cb1b00a5358f97a6fe47
Yuey - Laptop <byue@ualberta.ca>
2014-09-15 10:50:40 -0600

    Started working on a debug logger
    One of the only scenarios that are implemented using a singleton!

---
+++ TurboAwesomeRound2/TurboDebug.h
@@ -0,0 +1,38 @@
+/*
+    turbo-competition-helper
+    Copyright (C) 2014  Brandon Yue & Andrew Coulthard
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __TURBO_DEBUG_H_DEFINED__
+#define __TURBO_DEBUG_H_DEFINED__
+
+#define DEBUG
+#define DEBUGLEVEL
+
+class TurboDebugHelper
+{
+    public:
+        static TurboDebugHelper& getInstance()
+        {
+            static TurboDebugHelper instance;
+            return instance;
+        }
+    private:
+        TurboDebugHelper(){};
+        TurboDebugHelper(TurboDebugHelper const&); // Don't Implement
+        void operator=(TurboDebugHelper const&);   // Don't implement
+};
+
+#endif//__TURBO_DEBUG_H_DEFINED__
